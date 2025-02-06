c91a0cb3987d910aa81a111ae7169a0f1a17dc21
Johan Dahlborg <johan@dahlborg.com>
2025-02-06 11:59:51 +0100

    chore(gitignore): update .gitignore file to include common files and directories related to virtual environments, Python, IDEs, Flask, databases, logs, coverage reports, Jupyter Notebook, local development configuration, dependencies, and compiled files
    refactor(config.py): modify SQLALCHEMY_DATABASE_URI to use environment variables
    for database connection configuration instead of hardcoding values
    
    refactor(models.py): remove unused import statement for barnum in models.py
    
    build(requirements.txt): add new dependencies Flask-Migrate, Flask-Security-Too,
    Flask-SQLAlchemy, future, greenlet, Mako, mysql-connector-python, python-dotenv,
    setuptools, SQLAlchemy, and typing_extensions. Update existing dependencies
    versions for alembic, Flask, Flask-Login, Flask-Principal, Flask-WTF, and
    requests

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
 
 class ConfigDebug():
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:hejsan123@localhost/shop20220128'    # File-based SQL database
+    SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_DATABASE')}"
     SECRET_KEY = 'SDFA11#'
 
  # Flask-Mail SMTP server settings
